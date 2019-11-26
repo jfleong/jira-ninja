0c7f4b6110979211ce5d71e984501aba0b6c7bbf
J Leong <jason.f.leong@gmail.com>
2019-11-26 10:02:59 -1000

    adds a collapse if not visible function

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var collapseButton = document.getElementById('collapseSprintsButton');
-    collapseButton.onclick = sendCollapse;
+    var collapseAllButton = document.getElementById('collapseAllSprintsButton');
+    collapseButton.onclick = sendCollapseAll;
+    var collapseNoVisibleButton = document.getElementById('collapseNoVisibleButton');
+    collapseNoVisibleButton.onclick = sendCollapseNoVisible;
 });
 
-function sendCollapse() {
+function sendCollapseAll() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "collapseSprints" }, function (response) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "collapseAllSprints" }, function (response) {
+            console.log('who cares?');
+        });
+    });
+}
+
+function sendCollapseNoVisible() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "collapseNoVisibleIssueSprints" }, function (response) {
             console.log('who cares?');
         });
     });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,6 +1,9 @@
+/*
+ * Collapse all sprints
+ */
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.action == "collapseSprints") {
+        if (request.action == "collapseAllSprints") {
             collapseSprints(request, sender, sendResponse);
             // this is required to use sendResponse asynchronously
             return true;
@@ -8,10 +11,40 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-function collapseSprints(request, sender, sendResponse) {
+function collapseAllSprints(request, sender, sendResponse) {
   var x = document.querySelectorAll(".ghx-open .aui-iconfont-expanded");
    var i;
    for (i = 0; i < x.length; i++) {
      x[i].click();
    }
+}
+
+/*
+ * I wanted a way to collapse all sprints that had 0 visible issues because
+ * When doing sprint planning we look at individual users and I want to see
+ * all the tickets from all the sprints for an individual
+ */
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      if (request.action == "collapseNoVisibleIssueSprints") {
+          collapseNoVisibleIssueSprints(request, sender, sendResponse);
+          // this is required to use sendResponse asynchronously
+          return true;
+      }
+  }
+);
+
+/*
+ * If there is 0 of xxx issues visible then collapse, otherwise leave it open
+ */
+function collapseNoVisibleIssueSprints(request, sender, sendResponse) {
+  var x = document.querySelectorAll(".ghx-open .aui-iconfont-expanded");
+  var i
+  for (i = 0; i < x.length; i++) {
+    checkbox = x[i];
+    issue_count = checkbox.siblings(".ghx-issue-count")[0];
+    if (issue_count.innerHTML.toLowerCase().indexOf("0 ") === -1) {
+      x[i].click();
+    }
+  }
 }
